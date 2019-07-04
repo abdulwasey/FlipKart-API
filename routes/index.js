87a9fe3bc3ab1809f4cd4aa1823fe1a65d3d8084
Mohammed Abdul Wasey <abdulwasey13@gmail.com>
2019-07-04 22:57:58 +0530

    Data files

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var details = require("./details");
 var client = require('flipkart-api-affiliate-client');
-
+var _data = require('../lib/data');
 var fkClient = new client({
   trackingId: details.trackingId,
   token: details.token
@@ -31,14 +31,15 @@ router.post("/", function (req, res, next) {
     })
   }).then(function (value) {
     var listings = Object.keys(value.apiGroups.affiliate.apiListings);
-  fkClient.doKeywordSearch(search, 10).then(function (value) {
-    return new Promise(function (resolve, reject) {
-      resolve(JSON.parse(value.body));
+    fkClient.doKeywordSearch(search, 10).then(function (value) {
+      return new Promise(function (resolve, reject) {
+        resolve(JSON.parse(value.body));
+      })
+    }).then(function (value) {
+      //res.send(value);
+      res.render("index", { product: value.products, categories: listings });
     })
-  }).then(function (value) {
-    //res.send(value);
-    res.render("index", { product: value.products, categories: listings });
-  })});
+  });
 });
 
 
@@ -55,7 +56,22 @@ router.get("/:category", function (req, res, next) {
         resolve(JSON.parse(data.body));
       })
     }).then(function (data) {
-      res.render("index", { product: data.products, categories: listings });
+      var content = data;
+      _data.read('data', req.params.category, function (err, data) {
+        if (err) {
+          _data.create('data', req.params.category, content, function (err) {
+            if (!err) {
+              res.render("index", { product: content.products, categories: listings });
+            }
+            else {
+              res.send({ 'Error': 'Could not create a new file' })
+            }
+          });
+        }else{
+          res.render("index", { product: content.products, categories: listings });
+        }
+      });
+
     });
   });
 });
