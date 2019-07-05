981b2912613432d0e7f944f2844d39a50b2f5afa
Mohammed Abdul Wasey <abdulwasey13@gmail.com>
2019-07-05 18:56:41 +0530

    Comments

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,46 @@ var router = express.Router();
 var details = require("./details");
 var client = require('flipkart-api-affiliate-client');
 var _data = require('../lib/data');
+
+// Intializing the new client
 var fkClient = new client({
   trackingId: details.trackingId,
   token: details.token
 }, "json");
 
 
-
+/**
+ * Root Route:
+ * It will fetch all the categories available
+ */
 router.get("/", function (req, res, next) {
   var product = null;
+
   fkClient.getProductsFeedListing().then(function (value) {
     return new Promise(function (resolve, reject) {
       resolve(JSON.parse(value.body));
     })
   }).then(function (value) {
+
     var listings = Object.keys(value.apiGroups.affiliate.apiListings);
     res.render("index", { product, categories: listings });
   });
 });
 
-
+/**
+ * Search for the product using filpkarts built in search function
+ *
+ */
 router.post("/", function (req, res, next) {
   let search = req.body.search;
+
   fkClient.getProductsFeedListing().then(function (value) {
     return new Promise(function (resolve, reject) {
       resolve(JSON.parse(value.body));
     })
   }).then(function (value) {
     var listings = Object.keys(value.apiGroups.affiliate.apiListings);
+
     fkClient.doKeywordSearch(search, 10).then(function (value) {
       return new Promise(function (resolve, reject) {
         resolve(JSON.parse(value.body));
@@ -42,21 +54,29 @@ router.post("/", function (req, res, next) {
   });
 });
 
-
+/**
+ * Show all the products of a certain category
+ * Write the data into a json file and store on the server
+ * 
+ */
 router.get("/:category", function (req, res, next) {
+
   fkClient.getProductsFeedListing().then(function (value) {
     return new Promise(function (resolve, reject) {
       resolve(JSON.parse(value.body));
     })
   }).then(function (value) {
+
     var listings = Object.keys(value.apiGroups.affiliate.apiListings);
     var url = value.apiGroups.affiliate.apiListings[req.params.category].availableVariants['v1.1.0'].get;
+
     fkClient.getProductsFeed(url).then(function (data) {
       return new Promise(function (resolve, reject) {
         resolve(JSON.parse(data.body));
       })
     }).then(function (data) {
       var content = data;
+
       _data.read('data', req.params.category, function (err, data) {
         if (err) {
           _data.create('data', req.params.category, content, function (err) {
@@ -67,11 +87,11 @@ router.get("/:category", function (req, res, next) {
               res.send({ 'Error': 'Could not create a new file' })
             }
           });
-        }else{
+        }
+        else {
           res.render("index", { product: content.products, categories: listings });
         }
       });
-
     });
   });
 });
